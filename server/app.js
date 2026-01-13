16d44e23a278c2aa970125c2606097289c905822
Md-Asif-Azmi01 <asifazmi3549@gmail.com>
2026-01-13 17:45:26 +0530

    Fix CORS origin configuration

--- server/app.js
+++ server/app.js
@@ -15,7 +15,7 @@ app.use(cookieParser());
 
 app.use(cors(
  {
-        origin: ['https://fullstack-clint.onrender.com'] || ['http://localhost:5173'],
+        origin: ['https://fullstack-clint.onrender.com', 'http://localhost:5173'],
         credentials: true,
     }
 ));
